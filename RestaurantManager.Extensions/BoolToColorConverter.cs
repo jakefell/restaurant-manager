dc7f3b53f4ad8283f0cbb4a50572f8cee30673c7
Jake Fell <jakefell@hotmail.co.uk>
2015-12-09 22:55:19 +0000

    Advanced XAML Homework Module 3

---
+++ RestaurantManager.Extensions/BoolToColorConverter.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Data;
+
+namespace RestaurantManager.Extensions
+{
+    public class BoolToColorConverter : IValueConverter
+    {
+        public Color TrueColor { get; set; }
+
+        public Color FalseColor { get; set; }
+
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            Color result = Colors.Transparent;
+
+            if (value is bool)
+            {
+                result = (bool)value ? TrueColor : FalseColor;
+            }
+
+            return result;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            bool? result = null;
+
+            if (value is Color)
+            {
+                if ((Color)value == TrueColor)
+                    result = true;
+                else if ((Color)value == FalseColor)
+                    result = false;
+            }
+
+            return result;
+        }
+    }
+}
