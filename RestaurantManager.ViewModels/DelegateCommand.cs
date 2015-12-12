23356ce0f0fb3ed5e2fc955fbd06b9eb18ce6c0e
Jake Fell <jakefell@hotmail.co.uk>
2015-12-12 14:06:44 +0000

    Advanced XAML Homework Moudle 4

---
+++ RestaurantManager.ViewModels/DelegateCommand.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace RestaurantManager.ViewModels
+{
+    public class DelegateCommand : ICommand
+    {
+        private readonly Func<bool> _canExecuteMethod;
+        private readonly Action _executeMethod;
+
+        #region Constructors
+
+        public DelegateCommand(Action executeMethod)
+            : this(executeMethod, null)
+        {
+        }
+
+        public DelegateCommand(Action executeMethod, Func<bool> canExecuteMethod)
+        {
+            _executeMethod = executeMethod;
+            _canExecuteMethod = canExecuteMethod;
+        }
+
+        #endregion Constructors
+
+        #region ICommand Members
+
+        public event EventHandler CanExecuteChanged;
+
+        bool ICommand.CanExecute(object parameter)
+        {
+            try
+            {
+                return CanExecute();
+            }
+            catch { return false; }
+        }
+
+        void ICommand.Execute(object parameter)
+        {
+            Execute();
+        }
+
+        #endregion ICommand Members
+
+        #region Public Methods
+
+        public bool CanExecute()
+        {
+            return ((_canExecuteMethod == null) || _canExecuteMethod());
+        }
+
+        public void Execute()
+        {
+            if (_executeMethod != null)
+            {
+                _executeMethod();
+            }
+        }
+
+        public void RaiseCanExecuteChanged()
+        {
+            OnCanExecuteChanged(EventArgs.Empty);
+        }
+
+        #endregion Public Methods
+
+        #region Protected Methods
+
+        protected virtual void OnCanExecuteChanged(EventArgs e)
+        {
+            var handler = CanExecuteChanged;
+            if (handler != null)
+            {
+                handler(this, e);
+            }
+        }
+
+        #endregion Protected Methods
+    }
+}
---
+++ RestaurantManager.ViewModels/OrderViewModel.cs
@@ -0,0 +1,108 @@
+﻿using RestaurantManager.Models;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System;
+using Windows.UI.Popups;
+
+namespace RestaurantManager.ViewModels
+{
+    public class OrderViewModel : ViewModel
+    {
+        private MenuItem _selectedMenuItem;
+        private string _specialRequests;
+
+        private List<MenuItem> _menuItems;
+        private ObservableCollection<MenuItem> _currentlySelectedMenuItems;
+
+        public OrderViewModel()
+        {
+            this.AddMenuItemCommand = new DelegateCommand(AddMenuItem);
+            this.SubmitOrderCommand = new DelegateCommand(SubmitOrder);
+
+            this.CurrentlySelectedMenuItems = new ObservableCollection<MenuItem>();
+        }
+
+        protected override void OnDataLoaded()
+        {
+            this.MenuItems = base.Repository.StandardMenuItems;
+            this.SelectedMenuItem = this.MenuItems.First();
+        }
+
+        public DelegateCommand AddMenuItemCommand { get; private set; }
+        public DelegateCommand SubmitOrderCommand { get; private set; }
+
+        public MenuItem SelectedMenuItem
+        {
+            get { return _selectedMenuItem; }
+            set
+            {
+                if (_selectedMenuItem != value)
+                {
+                    _selectedMenuItem = value;
+                    NotifyPropertyChanged();
+                } 
+            }
+        }
+
+        public string SpecialRequests
+        {
+            get { return _specialRequests; }
+            set
+            {
+                if (_specialRequests != value)
+                {
+                    _specialRequests = value;
+                    NotifyPropertyChanged();
+                }
+            }
+        }
+
+        public List<MenuItem> MenuItems
+        {
+            get { return _menuItems; }
+            set
+            {
+                if (_menuItems != value)
+                {
+                    _menuItems = value;
+                    NotifyPropertyChanged();
+                }
+            }
+        }
+
+        public ObservableCollection<MenuItem> CurrentlySelectedMenuItems
+        {
+            get { return _currentlySelectedMenuItems; }
+            set
+            {
+                if (_currentlySelectedMenuItems != value)
+                {
+                    _currentlySelectedMenuItems = value;
+                    NotifyPropertyChanged();
+                }
+            }
+        }
+
+        private void AddMenuItem()
+        {
+            this.CurrentlySelectedMenuItems.Add(SelectedMenuItem);
+        }
+
+        private async void SubmitOrder()
+        {
+            base.Repository.Orders.Add(new Order
+            {
+                Complete = false,
+                Expedite = false,
+                Items = new List<MenuItem>(this.CurrentlySelectedMenuItems),
+                SpecialRequests = this.SpecialRequests,
+                Table = base.Repository.Tables.First()
+            });
+
+            this.CurrentlySelectedMenuItems.Clear();
+
+            await new MessageDialog("Order Submitted").ShowAsync();
+        }
+    }
+}
