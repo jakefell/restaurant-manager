84b421a67ef680fdab40429f2f4737d810bdb1b0
Jake Fell <jakefell@jackpotjoygroup.com>
2019-04-15 18:55:17 +0100

    First Commit

--- RestaurantManager.Models/Order.cs
+++ RestaurantManager.Models/Order.cs
@@ -20,6 +20,8 @@ namespace RestaurantManager.Models
 
         public bool Expedite { get; set; }
 
+        public string New { get; set; }
+
         public override string ToString()
         {
             return String.Join(", ", Items.Select(i => i.Title));
